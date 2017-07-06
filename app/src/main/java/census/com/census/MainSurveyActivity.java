f1947ab222a45a4e8594c3e62a0f35d7f018d7c5
Gene Christhopher Romuga <generomuga12693@gmail.com>
2017-07-06 10:34:02 +0800

    added functionality to call family and health fragments

--- app/src/main/java/census/com/census/MainSurveyActivity.java
+++ app/src/main/java/census/com/census/MainSurveyActivity.java
@@ -1,7 +1,10 @@
 package census.com.census;
 
+
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainSurveyActivity extends AppCompatActivity {
 
@@ -10,4 +13,24 @@ public class MainSurveyActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_survey);
     }
+
+    public void changeFragment(View view){
+        Fragment fragment;
+
+        switch (view.getId()){
+            case R.id.imageButtonFamily:
+                fragment = new FamilyFragment();
+                getSupportFragmentManager().beginTransaction()
+                        //.replace(R.id.fragmentMain,fragment,fragment.getClass().getSimpleName()).addToBackStack(null).commit();
+                        .replace(R.id.fragmentMain,fragment,null).commit();
+                break;
+            case R.id.imageButtonHealth:
+                fragment = new HealthFragment();
+                getSupportFragmentManager().beginTransaction()
+                        //.replace(R.id.fragmentMain,fragment,fragment.getClass().getSimpleName()).addToBackStack(null).commit();
+                        .replace(R.id.fragmentMain,fragment,null).commit();
+                break;
+        }
+    }
+
 }
