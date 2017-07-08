6ac8f813b085fc026a185cd81c7c820e35586a08
Gene Christhopher Romuga <generomuga12693@gmail.com>
2017-07-08 08:57:23 +0800

    added startActivity to go to the survey form

--- app/src/main/java/census/com/census/MainActivity.java
+++ app/src/main/java/census/com/census/MainActivity.java
@@ -1,14 +1,27 @@
 package census.com.census;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FloatingActionButton fabAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fabAdd = (FloatingActionButton) findViewById(R.id.fabAddSurvey);
+        fabAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,MainSurveyActivity.class));
+            }
+        });
     }
 
 }
