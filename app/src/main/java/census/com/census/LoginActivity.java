7cfe5ad4eea503e3be0bc4ebfa2baed1be540dce
Gene Christhopher Romuga <generomuga12693@gmail.com>
2017-07-08 09:01:10 +0800

    added intent to login to go to the main class

--- app/src/main/java/census/com/census/LoginActivity.java
+++ app/src/main/java/census/com/census/LoginActivity.java
@@ -1,13 +1,27 @@
 package census.com.census;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button btnSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnSignIn = (Button) findViewById(R.id.buttonSignIn);
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this,MainActivity.class));
+            }
+        });
+
     }
 }
