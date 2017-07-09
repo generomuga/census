7e15ff96bb4fdef27263becb0038391bd1528bb1
Gene Christhopher Romuga <generomuga12693@gmail.com>
2017-07-09 10:46:21 +0800

    refactor and added isp spinner to family fragment

--- app/src/main/java/census/com/census/FamilyFragment.java
+++ app/src/main/java/census/com/census/FamilyFragment.java
@@ -11,7 +11,10 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static census.com.census.R.*;
 
@@ -19,7 +22,10 @@ import static census.com.census.R.*;
 public class FamilyFragment extends Fragment {
 
     private Spinner spinnerYear;
+    private Spinner spinnerISP;
     private ArrayList years;
+    private List<String> isps;
+    private ArrayAdapter<String> spinnerArrayAdapter;
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
@@ -29,29 +35,51 @@ public class FamilyFragment extends Fragment {
         //get reference of widgets from XML layout
         View view = inflater.inflate(layout.fragment_family,container,false);
         spinnerYear = (Spinner) view.findViewById(id.spinnerYear);
+        spinnerISP = (Spinner) view.findViewById(id.spinnerISP);
 
-        //get spinner values
+        //get spinner values for years
         getYears();
-
-        //Initializing array adapter
-        ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(this.getActivity(), R.layout.spinner_item, years);
-        spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        useArrayAdapter(years);
         spinnerYear.setAdapter(spinnerArrayAdapter);
+
+        //get spinner values for isp
+        getISP();
+        useArrayAdapter((ArrayList) isps);
+        spinnerISP.setAdapter(spinnerArrayAdapter);
 
         //return inflater.inflate(layout.fragment_family, container, false);
         return  view;
     }
 
+    private void useArrayAdapter(ArrayList arrayList){
+        spinnerArrayAdapter = new ArrayAdapter<String>(this.getActivity(),R.layout.spinner_item,arrayList);
+        spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+    }
 
     @RequiresApi(api = Build.VERSION_CODES.N)
-    public void getYears(){
+    private void getYears(){
         years = new ArrayList<String>();
         int currentYear = Calendar.getInstance().get(Calendar.YEAR);
-        for(int year = 1990;year<=currentYear;year++){
+        for(int year = 1980;year<=currentYear;year++){
             years.add(Integer.toString(year));
         }
 
     }
+
+    private void getISP(){
+        isps = new ArrayList<>(Arrays.asList(
+                "PLDT/Smart/Sun",
+                "Globe/Innove",
+                "Eastern Telecoms",
+                "Bayantel",
+                "Wi-Tribe",
+                "Destiny Cable Internet",
+                "Sky Broadband"
+        ));
+
+    }
+
+
 
 
 
