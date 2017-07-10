b194b92bbb379e2fcbc21951f8727911028eeca5
Gene Christhopher Romuga <generomuga12693@gmail.com>
2017-07-10 13:23:21 +0800

    added environment fragment

--- app/src/main/java/census/com/census/EnvironmentFragment.java
+++ app/src/main/java/census/com/census/EnvironmentFragment.java
@@ -1,12 +1,12 @@
 package census.com.census;
 
-import android.content.Context;
-import android.net.Uri;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 
 public class EnvironmentFragment extends Fragment {
 
@@ -16,6 +16,5 @@ public class EnvironmentFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_environment, container, false);
     }
-
 
 }
