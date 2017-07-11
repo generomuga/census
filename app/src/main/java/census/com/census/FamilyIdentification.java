a98732d5eac0ef94e60f64d0742bffc399b52ac0
Gene Christhopher Romuga <generomuga12693@gmail.com>
2017-07-11 13:54:20 +0800

    connected to firebase

---
+++ app/src/main/java/census/com/census/FamilyIdentification.java
@@ -0,0 +1,15 @@
+package census.com.census;
+
+public class FamilyIdentification {
+
+    private String id;
+    private String fName;
+    private String mName;
+    private String lName;
+    private String houseNp;
+    private String streetNo;
+    private String barangay;
+    private String municipality;
+    private String province;
+
+}
--- app/src/main/java/census/com/census/FamilyIdentificationFragment.java
+++ app/src/main/java/census/com/census/FamilyIdentificationFragment.java
@@ -8,15 +8,12 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class FamilyIdentificationFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_family_identification, container, false);
     }
 
