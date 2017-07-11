1026361e426cac26f275da6b465fb943a6739c04
Gene Christhopher Romuga <generomuga12693@gmail.com>
2017-07-11 14:30:37 +0800

    complete init views

--- app/src/main/java/census/com/census/FamilyIdentificationFragment.java
+++ app/src/main/java/census/com/census/FamilyIdentificationFragment.java
@@ -7,6 +7,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -19,7 +21,17 @@ public class FamilyIdentificationFragment extends Fragment {
     private EditText editTextFName;
     private EditText editTextMName;
     private EditText editTextLName;
-
+    private EditText editTextHouseNo;
+    private EditText editTextStreetNo;
+    private EditText editTextBarangay;
+    private EditText editTextMunicipality;
+    private EditText editTextProvince;
+    private RadioButton radioButtonResident;
+    private RadioButton radioButtonNonResident;
+    private RadioButton radioButtonOwner;
+    private RadioButton radioButtonExtended;
+    private RadioButton radioButtonActive;
+    private RadioButton radioButtonInactive;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -33,7 +45,6 @@ public class FamilyIdentificationFragment extends Fragment {
         //firebase connection
         initFirebase();
 
-
         return view;
     }
 
@@ -41,11 +52,21 @@ public class FamilyIdentificationFragment extends Fragment {
         editTextFName = (EditText) view.findViewById(R.id.editTextFname);
         editTextMName = (EditText) view.findViewById(R.id.editTextMname);
         editTextLName = (EditText) view.findViewById(R.id.editTextLname);
+        editTextHouseNo = (EditText) view.findViewById(R.id.editTextHouseNo);
+        editTextStreetNo = (EditText) view.findViewById(R.id.editTextStreetNo);
+        editTextBarangay = (EditText) view.findViewById(R.id.editTextBrgy);
+        editTextMunicipality = (EditText) view.findViewById(R.id.editTextMunicipality);
+        editTextProvince = (EditText) view.findViewById(R.id.editTextProvince);
+        radioButtonResident = (RadioButton) view.findViewById(R.id.radioButtonResident);
+        radioButtonNonResident = (RadioButton) view.findViewById(R.id.radioButtonNonResident);
+        radioButtonOwner = (RadioButton) view.findViewById(R.id.radioButtonOwner);
+        radioButtonExtended = (RadioButton) view.findViewById(R.id.radioButtonExtended);
+        radioButtonActive = (RadioButton) view.findViewById(R.id.radioButtonActive);
+        radioButtonInactive = (RadioButton) view.findViewById(R.id.radioButtonInactive);
     }
 
     private void initFirebase(){
         dbSurvey = FirebaseDatabase.getInstance().getReference("survey");
     }
-
 
 }
