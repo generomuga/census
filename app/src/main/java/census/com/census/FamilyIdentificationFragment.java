1cb4b8eff39c270c3af8c890ef37d7fe995fd81c
Gene Christhopher Romuga <generomuga12693@gmail.com>
2017-07-11 14:14:28 +0800

    added firebase connection and init views in fragment family identification

--- app/src/main/java/census/com/census/FamilyIdentificationFragment.java
+++ app/src/main/java/census/com/census/FamilyIdentificationFragment.java
@@ -6,15 +6,46 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 public class FamilyIdentificationFragment extends Fragment {
+
+    private DatabaseReference dbSurvey;
+    private View view;
+    private EditText editTextFName;
+    private EditText editTextMName;
+    private EditText editTextLName;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_family_identification, container, false);
+        view = inflater.inflate(R.layout.fragment_family_identification, container, false);
+
+        //init views
+        initViews();
+
+        //firebase connection
+        initFirebase();
+
+
+        return view;
     }
+
+    private void initViews(){
+        editTextFName = (EditText) view.findViewById(R.id.editTextFname);
+        editTextMName = (EditText) view.findViewById(R.id.editTextMname);
+        editTextLName = (EditText) view.findViewById(R.id.editTextLname);
+    }
+
+    private void initFirebase(){
+        dbSurvey = FirebaseDatabase.getInstance().getReference("survey");
+    }
+
 
 }
