71de761e0f517466ca7402a4a35f0dfed19c7394
Gene Christhopher Romuga <generomuga12693@gmail.com>
2017-07-12 16:17:19 +0800

    added fragment selector validation

--- app/src/main/java/census/com/census/MainSurveyActivity.java
+++ app/src/main/java/census/com/census/MainSurveyActivity.java
@@ -1,7 +1,6 @@
 package census.com.census;
 
 
-import android.app.Activity;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,9 +10,21 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import java.util.Objects;
+
 public class MainSurveyActivity extends AppCompatActivity {
 
     private Toolbar toolBarSurvey;
+    String tag;
+    Fragment fragment;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        tag = "FamilyIdentification";
+        fragment = new FamilyIdentificationFragment();
+        switchFragment(fragment,tag);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,27 +37,31 @@ public class MainSurveyActivity extends AppCompatActivity {
     }
 
     public void changeFragment(View view){
-        Fragment fragment;
         switch (view.getId()){
             case R.id.imageButtonFamilyId:
+                tag = "FamilyIdentification";
                 getSupportActionBar().setTitle("Family Identification");
                 fragment = new FamilyIdentificationFragment();
-                switchFragment(fragment,"FamilyIdentification");
+                switchFragment(fragment,tag);
+                Toast.makeText(this,tag,Toast.LENGTH_SHORT).show();
                 break;
 
             case R.id.imageButtonFamily:
+                tag = "Family";
                 getSupportActionBar().setTitle("Family");
                 fragment = new FamilyFragment();
                 switchFragment(fragment,"Family");
                 break;
 
             case R.id.imageButtonHealth:
+                tag = "Health";
                 getSupportActionBar().setTitle("Health");
                 fragment = new HealthFragment();
                 switchFragment(fragment,"Health");
                 break;
 
             case R.id.imageButtonEnvironment:
+                tag = "Environment";
                 getSupportActionBar().setTitle("Environment");
                 fragment = new EnvironmentFragment();
                 switchFragment(fragment,"Environment");
@@ -70,18 +85,33 @@ public class MainSurveyActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_save:
-                checkActiveFragment();
+                checkActiveFragment(tag);
                 break;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
-    private void checkActiveFragment(){
-        Fragment fi =  getSupportFragmentManager().findFragmentByTag("Environment");
-        if (fi != null) {
-            if (fi.isVisible()) {
-                Toast.makeText(this, "Hi", Toast.LENGTH_LONG).show();
+    private void checkActiveFragment(String tag){
+        //Toast.makeText(this,tag,Toast.LENGTH_SHORT).show();
+        Fragment fragment =  getSupportFragmentManager().findFragmentByTag(tag);
+
+        if (fragment != null) {
+            if (fragment.isVisible()) {
+                switch (tag){
+                    case "FamilyIdentification":
+                        Toast.makeText(this,"FamilyIdentification",Toast.LENGTH_SHORT).show();
+                        break;
+                    case "Family":
+                        Toast.makeText(this,"Family",Toast.LENGTH_SHORT).show();
+                        break;
+                    case "Health":
+                        Toast.makeText(this,"Health",Toast.LENGTH_SHORT).show();
+                        break;
+                    case "Environment":
+                        Toast.makeText(this,"Environment",Toast.LENGTH_SHORT).show();
+                        break;
+                }
             } else {
                 Toast.makeText(this, "Hello", Toast.LENGTH_LONG).show();
             }
