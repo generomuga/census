df7c6f0c7c3923d4e744f2a1d6929be03c0af522
Gene Christhopher Romuga <generomuga12693@gmail.com>
2017-07-14 13:41:18 +0800

    added family object

---
+++ app/src/main/java/census/com/census/Family.java
@@ -0,0 +1,347 @@
+package census.com.census;
+
+public class Family {
+
+    private String id;
+    private int noFamilyMembers;
+    private int yearResided;
+    private String placeOrigin;
+    private String contactNo;
+    private String isp;
+    private int selectBicycle;
+    private int noBicycle;
+    private int selectBoat;
+    private int noBoat;
+    private int selectBus;
+    private int noBus;
+    private int selectCar;
+    private int noCar;
+    private int selectJeepney;
+    private int noJeepney;
+    private int selectMotorboat;
+    private int noMotorboat;
+    private int selectMotorcycle;
+    private int noMotorCycle;
+    private int selectOwnerJeep;
+    private int noOwnerJeep;
+    private int selectPedicab;
+    private int noPedicab;
+    private int selectPickup;
+    private int noPickup;
+    private int selectPumpBoat;
+    private int noPumpBoat;
+    private int selectRaft;
+    private int noRaft;
+    private int selectSuv;
+    private int noSuv;
+    private int selectTricycle;
+    private int noTricycle;
+    private int selectTruck;
+    private int noTruck;
+    private int selectVan;
+    private int noVan;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getNoFamilyMembers() {
+        return noFamilyMembers;
+    }
+
+    public void setNoFamilyMembers(int noFamilyMembers) {
+        this.noFamilyMembers = noFamilyMembers;
+    }
+
+    public int getYearResided() {
+        return yearResided;
+    }
+
+    public void setYearResided(int yearResided) {
+        this.yearResided = yearResided;
+    }
+
+    public String getPlaceOrigin() {
+        return placeOrigin;
+    }
+
+    public void setPlaceOrigin(String placeOrigin) {
+        this.placeOrigin = placeOrigin;
+    }
+
+    public String getContactNo() {
+        return contactNo;
+    }
+
+    public void setContactNo(String contactNo) {
+        this.contactNo = contactNo;
+    }
+
+    public String getIsp() {
+        return isp;
+    }
+
+    public void setIsp(String isp) {
+        this.isp = isp;
+    }
+
+    public int getSelectBicycle() {
+        return selectBicycle;
+    }
+
+    public void setSelectBicycle(int selectBicycle) {
+        this.selectBicycle = selectBicycle;
+    }
+
+    public int getNoBicycle() {
+        return noBicycle;
+    }
+
+    public void setNoBicycle(int noBicycle) {
+        this.noBicycle = noBicycle;
+    }
+
+    public int getSelectBoat() {
+        return selectBoat;
+    }
+
+    public void setSelectBoat(int selectBoat) {
+        this.selectBoat = selectBoat;
+    }
+
+    public int getNoBoat() {
+        return noBoat;
+    }
+
+    public void setNoBoat(int noBoat) {
+        this.noBoat = noBoat;
+    }
+
+    public int getSelectBus() {
+        return selectBus;
+    }
+
+    public void setSelectBus(int selectBus) {
+        this.selectBus = selectBus;
+    }
+
+    public int getNoBus() {
+        return noBus;
+    }
+
+    public void setNoBus(int noBus) {
+        this.noBus = noBus;
+    }
+
+    public int getSelectCar() {
+        return selectCar;
+    }
+
+    public void setSelectCar(int selectCar) {
+        this.selectCar = selectCar;
+    }
+
+    public int getNoCar() {
+        return noCar;
+    }
+
+    public void setNoCar(int noCar) {
+        this.noCar = noCar;
+    }
+
+    public int getSelectJeepney() {
+        return selectJeepney;
+    }
+
+    public void setSelectJeepney(int selectJeepney) {
+        this.selectJeepney = selectJeepney;
+    }
+
+    public int getNoJeepney() {
+        return noJeepney;
+    }
+
+    public void setNoJeepney(int noJeepney) {
+        this.noJeepney = noJeepney;
+    }
+
+    public int getSelectMotorboat() {
+        return selectMotorboat;
+    }
+
+    public void setSelectMotorboat(int selectMotorboat) {
+        this.selectMotorboat = selectMotorboat;
+    }
+
+    public int getNoMotorboat() {
+        return noMotorboat;
+    }
+
+    public void setNoMotorboat(int noMotorboat) {
+        this.noMotorboat = noMotorboat;
+    }
+
+    public int getSelectMotorcycle() {
+        return selectMotorcycle;
+    }
+
+    public void setSelectMotorcycle(int selectMotorcycle) {
+        this.selectMotorcycle = selectMotorcycle;
+    }
+
+    public int getNoMotorCycle() {
+        return noMotorCycle;
+    }
+
+    public void setNoMotorCycle(int noMotorCycle) {
+        this.noMotorCycle = noMotorCycle;
+    }
+
+    public int getSelectOwnerJeep() {
+        return selectOwnerJeep;
+    }
+
+    public void setSelectOwnerJeep(int selectOwnerJeep) {
+        this.selectOwnerJeep = selectOwnerJeep;
+    }
+
+    public int getNoOwnerJeep() {
+        return noOwnerJeep;
+    }
+
+    public void setNoOwnerJeep(int noOwnerJeep) {
+        this.noOwnerJeep = noOwnerJeep;
+    }
+
+    public int getSelectPedicab() {
+        return selectPedicab;
+    }
+
+    public void setSelectPedicab(int selectPedicab) {
+        this.selectPedicab = selectPedicab;
+    }
+
+    public int getNoPedicab() {
+        return noPedicab;
+    }
+
+    public void setNoPedicab(int noPedicab) {
+        this.noPedicab = noPedicab;
+    }
+
+    public int getSelectPickup() {
+        return selectPickup;
+    }
+
+    public void setSelectPickup(int selectPickup) {
+        this.selectPickup = selectPickup;
+    }
+
+    public int getNoPickup() {
+        return noPickup;
+    }
+
+    public void setNoPickup(int noPickup) {
+        this.noPickup = noPickup;
+    }
+
+    public int getSelectPumpBoat() {
+        return selectPumpBoat;
+    }
+
+    public void setSelectPumpBoat(int selectPumpBoat) {
+        this.selectPumpBoat = selectPumpBoat;
+    }
+
+    public int getNoPumpBoat() {
+        return noPumpBoat;
+    }
+
+    public void setNoPumpBoat(int noPumpBoat) {
+        this.noPumpBoat = noPumpBoat;
+    }
+
+    public int getSelectRaft() {
+        return selectRaft;
+    }
+
+    public void setSelectRaft(int selectRaft) {
+        this.selectRaft = selectRaft;
+    }
+
+    public int getNoRaft() {
+        return noRaft;
+    }
+
+    public void setNoRaft(int noRaft) {
+        this.noRaft = noRaft;
+    }
+
+    public int getSelectSuv() {
+        return selectSuv;
+    }
+
+    public void setSelectSuv(int selectSuv) {
+        this.selectSuv = selectSuv;
+    }
+
+    public int getNoSuv() {
+        return noSuv;
+    }
+
+    public void setNoSuv(int noSuv) {
+        this.noSuv = noSuv;
+    }
+
+    public int getSelectTricycle() {
+        return selectTricycle;
+    }
+
+    public void setSelectTricycle(int selectTricycle) {
+        this.selectTricycle = selectTricycle;
+    }
+
+    public int getNoTricycle() {
+        return noTricycle;
+    }
+
+    public void setNoTricycle(int noTricycle) {
+        this.noTricycle = noTricycle;
+    }
+
+    public int getSelectTruck() {
+        return selectTruck;
+    }
+
+    public void setSelectTruck(int selectTruck) {
+        this.selectTruck = selectTruck;
+    }
+
+    public int getNoTruck() {
+        return noTruck;
+    }
+
+    public void setNoTruck(int noTruck) {
+        this.noTruck = noTruck;
+    }
+
+    public int getSelectVan() {
+        return selectVan;
+    }
+
+    public void setSelectVan(int selectVan) {
+        this.selectVan = selectVan;
+    }
+
+    public int getNoVan() {
+        return noVan;
+    }
+
+    public void setNoVan(int noVan) {
+        this.noVan = noVan;
+    }
+}
