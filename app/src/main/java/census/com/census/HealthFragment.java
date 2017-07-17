46c3a18cd19f037106c3fbede00b8922258ff1e1
Gene Christhopher Romuga <generomuga12693@gmail.com>
2017-07-17 11:59:14 +0800

    init views for health fragg

--- app/src/main/java/census/com/census/HealthFragment.java
+++ app/src/main/java/census/com/census/HealthFragment.java
@@ -5,17 +5,39 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioButton;
 
 public class HealthFragment extends Fragment {
 
     View view;
 
+    public static RadioButton radioButtonEatYes;
+    public static RadioButton radioButtonEatNo;
+    public static RadioButton radioButtonHerbalYes;
+    public static RadioButton radioButtonHerbalNo;
+    public static RadioButton radioButtonVegYes;
+    public static RadioButton radioButtonVegNo;
+    public static RadioButton radioButtonIodizeYes;
+    public static RadioButton radioButtonIodizeNo;
+    public static RadioButton radioButtonFamilyYes;
+    public static RadioButton radioButtonFamilyNo;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_health, container, false);
+
+        //init views
+        initViews();
+
         return view;
     }
 
+    private void initViews(){
+        radioButtonEatYes = (RadioButton) view.findViewById(R.id.radioButtonEatYes);
+        radioButtonEatNo = (RadioButton) view.findViewById(R.id.radioButtonEatNo);
+        radioButtonHerbalYes = (RadioButton) view.findViewById(R.id.radioButtonHerbalYes);
+    }
 
 }
