74906e2dc47015ce4d5babe70b62aad87128345a
Gene Christhopher Romuga <generomuga12693@gmail.com>
2017-07-17 10:59:18 +0800

    family fragment init view

--- app/src/main/java/census/com/census/FamilyFragment.java
+++ app/src/main/java/census/com/census/FamilyFragment.java
@@ -9,6 +9,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -25,9 +27,31 @@ import static census.com.census.R.*;
 public class FamilyFragment extends Fragment {
 
     private View view;
+
+    public static CheckBox checkBoxBicycle;
+    public static CheckBox checkBoxBoat;
+    public static CheckBox checkBoxBus;
+    public static CheckBox checkBoxCar;
+    public static CheckBox checkBoxJeep;
+    public static CheckBox checkBoxMotorBoat;
+    public static CheckBox checkBoxMotorcycle;
+    public static CheckBox checkBoxOwner;
+    public static CheckBox checkBoxPedicab;
+    public static CheckBox checkBoxPickup;
+    public static CheckBox checkBoxPumpBoat;
+    public static CheckBox checkBoxRaft;
+    public static CheckBox checkBoxSuv;
+    public static CheckBox checkBoxTricycle;
+    public static CheckBox checkBoxTruck;
+    public static CheckBox checkBoxVan;
+
+    public static EditText editTextOrigin;
+    public static EditText editTextContactNo;
+
     public static Spinner spinnerYear;
     public static Spinner spinnerISP;
     public static SeekBar seekBarNoFamMembers;
+
     private static TextView textViewNoFamMembers;
 
     private ArrayList years;
@@ -44,22 +68,7 @@ public class FamilyFragment extends Fragment {
         initViews();
 
         //get the seekbar value
-        seekBarNoFamMembers.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                textViewNoFamMembers.setText(String.valueOf(progress));
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        });
+        getFamilyNoSeekBarVal();
 
         //get spinner values for years
         getYears();
@@ -75,8 +84,28 @@ public class FamilyFragment extends Fragment {
     }
 
     private void initViews(){
+        editTextOrigin = (EditText) view.findViewById(id.editTextOrigin);
+        editTextContactNo = (EditText) view.findViewById(id.editTextContactNo);
+
         spinnerYear = (Spinner) view.findViewById(id.spinnerYear);
         spinnerISP = (Spinner) view.findViewById(id.spinnerISP);
+
+        checkBoxBicycle = (CheckBox) view.findViewById(id.checkboxBicycle);
+        checkBoxBoat = (CheckBox) view.findViewById(id.checkboxBoat);
+        checkBoxBus = (CheckBox) view.findViewById(id.checkboxBus);
+        checkBoxCar = (CheckBox) view.findViewById(id.checkboxCar);
+        checkBoxJeep = (CheckBox) view.findViewById(id.checkboxJeepney);
+        checkBoxMotorBoat = (CheckBox) view.findViewById(id.checkboxMotorBoat);
+        checkBoxMotorcycle = (CheckBox) view.findViewById(id.checkboxMotorcycle);
+        checkBoxOwner = (CheckBox) view.findViewById(id.checkboxOwner);
+        checkBoxPedicab = (CheckBox) view.findViewById(id.checkboxPedicab);
+        checkBoxPickup = (CheckBox) view.findViewById(id.checkboxPickup);
+        checkBoxPumpBoat = (CheckBox) view.findViewById(id.checkboxPumpBoat);
+        checkBoxRaft = (CheckBox) view.findViewById(id.checkboxRaft);
+        checkBoxSuv = (CheckBox) view.findViewById(id.checkboxSuv);
+        checkBoxTricycle = (CheckBox) view.findViewById(id.checkboxTricycle);
+        checkBoxTruck = (CheckBox) view.findViewById(id.checkboxTruck);
+        checkBoxVan = (CheckBox) view.findViewById(id.checkboxVan);
 
         seekBarNoFamMembers = (SeekBar) view.findViewById(id.seekBarNoFamilyMembers);
         textViewNoFamMembers = (TextView) view.findViewById(id.textViewNoFamMembers);
@@ -106,6 +135,25 @@ public class FamilyFragment extends Fragment {
                 "Destiny Cable Internet",
                 "Sky Broadband"
         ));
+    }
+
+    private void getFamilyNoSeekBarVal(){
+        seekBarNoFamMembers.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textViewNoFamMembers.setText(String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 
 }
