8627ce4f762cae3c0388930cca4f3190a2d084e3
Gene Christhopher Romuga <generomuga12693@gmail.com>
2017-07-22 13:19:32 +0800

    fixed spinner item on fragment fi

--- app/src/main/java/census/com/census/FamilyIdentificationFragment.java
+++ app/src/main/java/census/com/census/FamilyIdentificationFragment.java
@@ -39,9 +39,6 @@ public class FamilyIdentificationFragment extends Fragment {
     public static RadioButton radioButtonInactive;
     public static Spinner spinnerRegions;
     public static Spinner spinnerProvinces;
-    private ArrayList regions;
-    private ArrayList provinces;
-    private ArrayList region4AProvince;
     private ArrayAdapter spinnerArrayAdapter;
 
     @Override
@@ -51,22 +48,8 @@ public class FamilyIdentificationFragment extends Fragment {
         //init views
         initViews();
 
-        //to load regions from sqlite
-        ArrayList listRegion;
-        String query = "SELECT DISTINCT(region) FROM locations";
-        listRegion = queryLocation(query,"region");
-
-        if(!listRegion.isEmpty()) {
-            useArrayAdapter(listRegion);
-            spinnerRegions.setAdapter(spinnerArrayAdapter);
-        }
-        else{
-            Log.i("List region","empty");
-        }
-
-        //select region
-        spinnerRegionEvent();
-        //spinnerProvinceEvent();
+        //to load regions from sql
+        loadRegions();
 
         return view;
     }
@@ -77,9 +60,6 @@ public class FamilyIdentificationFragment extends Fragment {
         editTextLName = (EditText) view.findViewById(R.id.editTextLname);
         editTextHouseNo = (EditText) view.findViewById(R.id.editTextHouseNo);
         editTextStreetNo = (EditText) view.findViewById(R.id.editTextStreetNo);
-        //editTextBarangay = (EditText) view.findViewById(R.id.editTextBrgy);
-        //editTextMunicipality = (EditText) view.findViewById(R.id.editTextMunicipality);
-        //editTextProvince = (EditText) view.findViewById(R.id.editTextProvince);
         radioButtonResident = (RadioButton) view.findViewById(R.id.radioButtonResident);
         radioButtonNonResident = (RadioButton) view.findViewById(R.id.radioButtonNonResident);
         radioButtonOwner = (RadioButton) view.findViewById(R.id.radioButtonOwner);
@@ -90,43 +70,60 @@ public class FamilyIdentificationFragment extends Fragment {
         spinnerProvinces = (Spinner) view.findViewById(R.id.spinnerProvince);
     }
 
-    private void useArrayAdapter(ArrayList arrayList){
-        spinnerArrayAdapter = new ArrayAdapter<String>(getActivity(),R.layout.spinner_item,arrayList);
-        spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+    private void loadRegions(){
+        ArrayList listRegion;
+        String query = "SELECT DISTINCT(region) FROM locations";
+        listRegion = queryLocation(query,"region");
+
+        if(!listRegion.isEmpty()) {
+            useArrayAdapter(listRegion);
+            spinnerRegions.setAdapter(spinnerArrayAdapter);
+        }
+        else{
+            Log.i("regions:","empty");
+        }
+
+        //select region
+        spinnerRegionEvent();
     }
 
     private void spinnerRegionEvent(){
         spinnerRegions.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(getActivity(),spinnerRegions.getSelectedItem().toString(),Toast.LENGTH_SHORT).show();
-
-                String query = "SELECT province FROM locations where region='"+spinnerRegions.getSelectedItem().toString()+"'";
-
-                ArrayList listProvince;
-                listProvince = queryLocation(query,"province");
-                if(!listProvince.isEmpty()) {
-                    useArrayAdapter(listProvince);
-                    spinnerProvinces.setAdapter(spinnerArrayAdapter);
-                }
-                else{
-                    Log.i("List region","empty");
-                }
+                getProvinces(spinnerRegions.getSelectedItem().toString().trim());
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-
+                //do nothing
             }
         });
     }
 
+    private void getProvinces(String region){
+        String query = "SELECT province FROM locations where region='"+region+"'";
+        ArrayList listProvince;
+        listProvince = queryLocation(query,"province");
+        if(!listProvince.isEmpty()) {
+            useArrayAdapter(listProvince);
+            spinnerProvinces.setAdapter(spinnerArrayAdapter);
+        }
+        else{
+            Log.i("provinces:","empty");
+        }
+    }
+
+    private void useArrayAdapter(ArrayList arrayList){
+        spinnerArrayAdapter = new ArrayAdapter<String>(getActivity(),R.layout.spinner_item,arrayList);
+        spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+    }
 
     public ArrayList queryLocation(String query,String toQuery){
         ArrayList listResult = new ArrayList<>();
+
         try {
             SQLiteDatabase dbLocation = getActivity().openOrCreateDatabase("locations", Context.MODE_PRIVATE, null);
-            //String query = "SELECT distinct(region) FROM locations";
 
             Cursor c = dbLocation.rawQuery(query,null);
 
@@ -139,7 +136,6 @@ public class FamilyIdentificationFragment extends Fragment {
                 c.moveToNext();
             }
 
-
             dbLocation.close();
         }
         catch (Exception e){
@@ -147,38 +143,5 @@ public class FamilyIdentificationFragment extends Fragment {
         }
         return listResult;
     }
-
-    /*private void createDbConnection(){
-        DbUtils dbUtils = new DbUtils(getActivity());
-        dbUtils.createDbConnection();
-    }
-
-    public void queryRegion(){
-        try {
-
-            listRegion = new ArrayList<>();
-
-            SQLiteDatabase dbLocation = getActivity().openOrCreateDatabase("locations", Context.MODE_PRIVATE, null);
-            String query = "SELECT distinct(region) FROM locations";
-
-            Cursor c = dbLocation.rawQuery(query,null);
-
-            int toQueryIndex = c.getColumnIndex("region");
-
-            c.moveToFirst();
-            while(c!=null){
-                Log.i("region",c.getString(toQueryIndex));
-                listRegion.add("REGION "+c.getString(toQueryIndex));
-                c.moveToNext();
-            }
-
-            dbLocation.close();
-        }
-        catch (Exception e){
-            Log.i("error",e.toString());
-        }
-    }
-    */
-
 
 }
