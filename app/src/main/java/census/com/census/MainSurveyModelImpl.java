f6ebd689c59ceddee314d8be33e4562f8b6d9eeb
Gene Christhopher Romuga <generomuga12693@gmail.com>
2017-08-04 10:45:21 +0800

    firebase add data on fi

--- app/src/main/java/census/com/census/MainSurveyModelImpl.java
+++ app/src/main/java/census/com/census/MainSurveyModelImpl.java
@@ -12,11 +12,19 @@ public class MainSurveyModelImpl implements MainSurveyModel {
 
     public MainSurveyModelImpl(OnFamilyIdentificationModel listener) {
         this.listener = listener;
+        mDatabase = FirebaseDatabase.getInstance().getReference("data");
     }
 
     @Override
     public void sendFirebase(String fname) {
-        mDatabase = FirebaseDatabase.getInstance().getReference("first");
-        mDatabase.setValue(fname);
+        FamilyIdentification familyIdentification = new FamilyIdentification();
+        familyIdentification.setfName(fname);
+
+        DatabaseReference mFamilyIdentification = mDatabase.child("familyIdentification");
+        mFamilyIdentification.push().setValue(familyIdentification);
+
+        //mDatabase = FirebaseDatabase.getInstance().getReference("family_identification");
+        //mDatabase.setValue(familyIdentification);
+
     }
 }
