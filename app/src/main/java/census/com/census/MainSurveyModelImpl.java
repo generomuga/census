20a68248174122f3e137ef002042166b2c25cfb2
Gene Christhopher Romuga <generomuga12693@gmail.com>
2017-08-04 09:59:54 +0800

    added firebase

--- app/src/main/java/census/com/census/MainSurveyModelImpl.java
+++ app/src/main/java/census/com/census/MainSurveyModelImpl.java
@@ -1,9 +1,14 @@
 package census.com.census;
 
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainSurveyModelImpl implements MainSurveyModel {
 
     OnFamilyIdentificationModel listener;
+
+    DatabaseReference mDatabase;
 
     public MainSurveyModelImpl(OnFamilyIdentificationModel listener) {
         this.listener = listener;
@@ -11,6 +16,7 @@ public class MainSurveyModelImpl implements MainSurveyModel {
 
     @Override
     public void sendFirebase(String fname) {
-
+        mDatabase = FirebaseDatabase.getInstance().getReference("first");
+        mDatabase.setValue(fname);
     }
 }
