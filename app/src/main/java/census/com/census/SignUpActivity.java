51567b7e16fd914aa82294cd7c1d111528cf7b85
Gene Christhopher Romuga <generomuga12693@gmail.com>
2017-08-05 20:13:22 +0800

    added reg user

--- app/src/main/java/census/com/census/SignUpActivity.java
+++ app/src/main/java/census/com/census/SignUpActivity.java
@@ -1,10 +1,17 @@
 package census.com.census;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUpActivity extends AppCompatActivity implements SignUpView{
 
@@ -12,18 +19,22 @@ public class SignUpActivity extends AppCompatActivity implements SignUpView{
     private Button mRegister;
     SignUpPresenter signUpPresenterListener;
 
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+        mAuth = FirebaseAuth.getInstance();
+
         mEmail = (EditText) findViewById(R.id.userEmail);
         mRegister = (Button) findViewById(R.id.buttonRegister);
         mRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                attemptRegister();
+                //attemptRegister();
+                register();
             }
         });
 
@@ -43,5 +54,19 @@ public class SignUpActivity extends AppCompatActivity implements SignUpView{
     @Override
     public void onSuccess() {
 
+    }
+
+
+    private void register(){
+
+        mAuth.createUserWithEmailAndPassword(mEmail.getText().toString().toString(),"geasdasd").addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (!task.isSuccessful()) {
+                    Log.e("firebase","faield");
+                }
+            }
+
+        });
     }
 }
--- app/src/main/java/census/com/census/SignUpModelImpl.java
+++ app/src/main/java/census/com/census/SignUpModelImpl.java
@@ -1,14 +1,16 @@
 package census.com.census;
 
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.util.Log;
 
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.concurrent.Executor;
 
 public class SignUpModelImpl implements SignUpModel{
 
@@ -24,11 +26,13 @@ public class SignUpModelImpl implements SignUpModel{
 
     @Override
     public void register(String email, String password) {
-        mAuth.createUserWithEmailAndPassword(email,password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+        /*mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
-            public void onSuccess(AuthResult authResult) {
-                //
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (!task.isSuccessful()) {
+                    Log.e("firebase","faield");
+                }
             }
-        });
+        });*/
     }
 }
