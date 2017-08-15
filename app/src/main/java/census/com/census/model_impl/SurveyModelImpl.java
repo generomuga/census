dbced43d5e59620ba0fc235993c3063a746ae576
Gene Christhopher Romuga <generomuga12693@gmail.com>
2017-08-15 16:30:05 +0800

    refactor family model

--- app/src/main/java/census/com/census/model_impl/SurveyModelImpl.java
+++ app/src/main/java/census/com/census/model_impl/SurveyModelImpl.java
@@ -34,7 +34,6 @@ public class SurveyModelImpl implements SurveyModel.OnFamilyIdentification,Surve
         key = mDatabase.push().getKey();
 
         String time = DateFormat.getDateTimeInstance().format(new Date());
-        Log.i("time",time);
 
         FamilyIdentification familyIdentification = new FamilyIdentification();
         familyIdentification.setId(key);
@@ -71,9 +70,6 @@ public class SurveyModelImpl implements SurveyModel.OnFamilyIdentification,Surve
     public void sendData(int familyNo, int yearReside, String region, String province, String municipality, String barangay, String isp, int bicycle, int qBicycle, int boat, int qBoat, int bus, int qBus, int car, int qCar, int jeep, int qJeep, int motorboat, int qMotorboat, int motorcycle, int qMotorcyle, int owner, int qOwner, int pedicab, int qPedicab, int pickup, int qPickup, int pumpboat, int qPumpboat, int raft, int qRaft, int suv, int qSuv, int tric, int qTric, int truck, int qTruck, int van, int qVan) {
 
         String time = DateFormat.getDateTimeInstance().format(new Date());
-        Log.i("time",time);
-
-
 
         Family family = new Family();
         family.setId(key);
