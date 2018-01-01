420c7bf817950e6c0ceb4154703ecf345d8ecfca
Romuga <GRomuga@cgiar.org>
2018-01-01 13:31:05 +0800

    Add fragment family identification

--- app/src/main/java/census/com/census/activity/MainSurveyActivity.java
+++ app/src/main/java/census/com/census/activity/MainSurveyActivity.java
@@ -1,15 +1,20 @@
 package census.com.census.activity;
 
 
+import android.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import census.com.census.R;
+import census.com.census.fragment.FamilyIdentificationFragment;
 
 public class MainSurveyActivity extends AppCompatActivity {
 
     //views
-    private Toolbar toolBarSurvey;
+    private Toolbar mToolBarSurvey;
+
+    FamilyIdentificationFragment mIdentification;
+    private String mTag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +22,17 @@ public class MainSurveyActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main_survey);
 
         //to add toolbar in the activity
-        toolBarSurvey = (Toolbar) findViewById(R.id.toolBarSurvey);
-        setSupportActionBar(toolBarSurvey);
+        mToolBarSurvey = (Toolbar) findViewById(R.id.toolBarSurvey);
+        setSupportActionBar(mToolBarSurvey);
         getSupportActionBar().setTitle("Identification");
+
+        //fragment
+        if (savedInstanceState == null) {
+            mTag = "familyIdentification";
+            mIdentification = new FamilyIdentificationFragment();
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragmentMain,mIdentification,mTag).commit();
+        }
 
     }
 
