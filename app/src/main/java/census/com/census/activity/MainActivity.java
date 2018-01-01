b821180b1a660b4118b8db69bdd3a873d030f018
Gene Romuga <generomuga12693@gmail.com>
2018-01-01 08:22:08 +0800

    Update login xml

--- app/src/main/java/census/com/census/activity/MainActivity.java
+++ app/src/main/java/census/com/census/activity/MainActivity.java
@@ -78,15 +78,12 @@ public class MainActivity extends AppCompatActivity implements MainView {
 
         surveyList = new ArrayList<>();
 
-        //connect to sqlite database
-        connectDB();
-
         //for toolbar init
-        toolbarMain = (Toolbar) findViewById(R.id.toolBarMain);
+        /*toolbarMain = (Toolbar) findViewById(R.id.toolBarMain);
         setSupportActionBar(toolbarMain);
         getSupportActionBar().setTitle("Main");
 
-        mFab = (ImageButton) findViewById(R.id.fab);
+        /*mFab = (ImageButton) findViewById(R.id.fab);
         mFab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -101,7 +98,7 @@ public class MainActivity extends AppCompatActivity implements MainView {
                 showUpdateDialog(familyIdentification.getId(),familyIdentification.getfName()+' '+familyIdentification.getlName());
                 return false;
             }
-        });
+        });*/
 
     }
 
@@ -165,23 +162,6 @@ public class MainActivity extends AppCompatActivity implements MainView {
         }
         else{
             finish();
-        }
-    }
-
-    private void connectDB(){
-        DatabaseHelper databaseHelper = new DatabaseHelper(MainActivity.this);
-        try {
-            databaseHelper.createDatabase();
-        }
-        catch (IOException e) {
-            Log.e("Error:","Unable to connect database");
-        }
-
-        try{
-            databaseHelper.openDataBase();
-        }
-        catch (SQLException sqle){
-            Log.e("Error:",sqle.toString());
         }
     }
 
