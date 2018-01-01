761d8f69c332df79d9ed22ce36f2d9cf0b70abb6
Romuga <GRomuga@cgiar.org>
2018-01-01 17:48:22 +0800

    Update allow to change fragment

--- app/src/main/java/census/com/census/activity/MainSurveyActivity.java
+++ app/src/main/java/census/com/census/activity/MainSurveyActivity.java
@@ -2,18 +2,26 @@ package census.com.census.activity;
 
 
 import android.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageButton;
+
 import census.com.census.R;
+import census.com.census.fragment.FamilyFragment;
 import census.com.census.fragment.FamilyIdentificationFragment;
 
 public class MainSurveyActivity extends AppCompatActivity {
 
     //views
     private Toolbar mToolBarSurvey;
+    private ImageButton mFamilyIdentification;
 
-    FamilyIdentificationFragment mIdentification;
+    //fragments
+    FragmentTransaction mTransaction;
+
     private String mTag;
 
     @Override
@@ -23,17 +31,43 @@ public class MainSurveyActivity extends AppCompatActivity {
 
         //to add toolbar in the activity
         mToolBarSurvey = (Toolbar) findViewById(R.id.toolBarSurvey);
+
         setSupportActionBar(mToolBarSurvey);
         getSupportActionBar().setTitle("Identification");
 
         //fragment
-        if (savedInstanceState == null) {
-            mTag = "familyIdentification";
-            mIdentification = new FamilyIdentificationFragment();
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.fragmentMain,mIdentification,mTag).commit();
+        if (savedInstanceState != null){
+            return;
         }
 
+        //set default fragment
+        FamilyIdentificationFragment familyIdentificationFragment = new FamilyIdentificationFragment();
+        familyIdentificationFragment.setArguments(getIntent().getExtras());
+        mTransaction = getSupportFragmentManager().beginTransaction();
+        mTransaction.add(R.id.fragmentMain, familyIdentificationFragment).commit();
     }
+
+    public void changeFragment(View view){
+
+        mTransaction = getSupportFragmentManager().beginTransaction();
+
+        switch (view.getId()){
+
+            case R.id.imageButtonFamilyId:
+                FamilyIdentificationFragment familyIdentificationFragment = new FamilyIdentificationFragment();
+                mTransaction.replace(R.id.fragmentMain, familyIdentificationFragment);
+                mTransaction.addToBackStack(null);
+                mTransaction.commit();
+                break;
+
+            case R.id.imageButtonFamily:
+                FamilyFragment familyFragment = new FamilyFragment();
+                mTransaction.replace(R.id.fragmentMain, familyFragment);
+                mTransaction.addToBackStack(null);
+                mTransaction.commit();
+                break;
+        }
+    }
+
 
 }
--- app/src/main/java/census/com/census/fragment/FamilyIdentificationFragment.java
+++ app/src/main/java/census/com/census/fragment/FamilyIdentificationFragment.java
@@ -1,25 +1,11 @@
 package census.com.census.fragment;
 
-import android.content.Context;
-import android.content.SharedPreferences;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.text.InputFilter;
-import android.text.Spanned;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.Spinner;
-import java.util.ArrayList;
-import java.util.Arrays;
-
-import census.com.census.DbUtils;
 import census.com.census.R;
 
 public class FamilyIdentificationFragment extends Fragment {
